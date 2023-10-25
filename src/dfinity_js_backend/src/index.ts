1198e4ff6723ecffac6a573c4bda3695558a510d
emmanuel <Emmanuelsumeh@gmail.com>
2023-10-25 10:19:14 +0100

    feat: boilerplate code

---
+++ src/dfinity_js_backend/src/index.ts
@@ -0,0 +1,1 @@
+// canister code goes here
---
+++ src/dfinity_js_frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+const App = function AppWrapper() {
+
+
+  return (
+      <>
+        <h1 className="text-center">ICP 201 Boilerplate</h1>
+      </>
+  );
+};
+
+export default App;
